673cd35bcda833f84fbcebf1d658dbdd7b35454e
李成才 <licca@jiedaibao.com>
2019-04-29 11:32:10 +0800

    first commit

---
+++ app/src/main/java/c/cc/viewdemo/view/CircleImageView.java
@@ -0,0 +1,32 @@
+package c.cc.viewdemo.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+/**
+ * @author lichengcai
+ * @class name：c.cc.viewdemo.view
+ * @date 2019/4/29 11:30
+ * @desc ${描述类实现的功能}
+ */
+public class CircleImageView extends android.support.v7.widget.AppCompatImageView {
+    public CircleImageView(Context context) {
+        super(context);
+    }
+
+    public CircleImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        
+        super.onDraw(canvas);
+    }
+}
